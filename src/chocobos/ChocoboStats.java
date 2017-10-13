a9ab963392829ea094d684919d9dba03a5583f97
ejaguete <erica.aguete@gmail.com>
2017-10-12 19:39:34 -0600

    version 1.4, converted stat fields to int, added new constructor for ChocoboStats

--- src/chocobos/ChocoboStats.java
+++ src/chocobos/ChocoboStats.java
@@ -5,33 +5,37 @@ import java.util.Map;
 
 public class ChocoboStats {
 	//  stats
-	private Map<String,Integer> stats = new HashMap<String,Integer>();
+	private Map<Integer,Integer> stats = new HashMap<Integer,Integer>();
 	
-	public static final String ST = "stamina"; // ST > 50 increases price
-	public static final String TMP = "temper"; // TMP > 5 harder to manage, more likely to buck
-	public static final String TYPE = "type"; 
-	public static final String HEAL = "healing";
-	public static final String DEF = "defense"; 
-	public static final String ATK = "attack";
+	public static final int ST = 0; // ST > 50 increases price
+	public static final int TMP = 1; // TMP > 5 harder to manage, more likely to buck
+	public static final int TYPE = 2; 
+	public static final int HEAL = 3;
+	public static final int DEF = 4; 
+	public static final int ATK = 5;
 	
 	public ChocoboStats() {	
 		// initialize battle stats
-		stats.put(ST, 0);
-		stats.put(TMP, 0);
-		stats.put(TYPE, 0);
-		stats.put(HEAL, 0);
-		stats.put(DEF, 0);
-		stats.put(ATK, 0);
+		for (int i=0;i<stats.size();++i)
+			set(i, 0);
 	}
 	
-	public int set(String stat, int value) {
-		stat = stat.toLowerCase();
+	public ChocoboStats(int[] newStats) {
+		if(newStats.length!=stats.size())
+			throw new IllegalArgumentException("Wrong number of arguments, requires " + stats.size());
+		
+		for(int i=0;i<stats.size();++i) {
+			set(i,newStats[i]);
+		}
+		
+	}
+	
+	public int set(int stat, int value) {
 		stats.put(stat, value);
 		return value;
 	}
 	
-	public int get(String stat) {
-		stat = stat.toLowerCase();
+	public int get(int stat) {
 		return stats.get(stat);
 	}
 
